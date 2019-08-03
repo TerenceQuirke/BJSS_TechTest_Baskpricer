5bf00d3569af8e5f0a26aaa728269aa80ca48ef5
quirket <terrence.quirke@umusic.com>
2019-08-03 12:53:32 +0100

    Initial Project structure built from Spring initialiser https://start.spring.io

---
+++ src/main/java/com/bjss/techtest/baskpricer/BaskpricerApplication.java
@@ -0,0 +1,13 @@
+package com.bjss.techtest.baskpricer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaskpricerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaskpricerApplication.class, args);
+	}
+
+}
